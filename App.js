b931ecb861cc6a4ae85773db6de698801940f795
tandamandali <tanmaykadam68@google.com>
2024-01-02 14:19:39 +0530

    Understanding the basics of react

---
+++ App.js
@@ -0,0 +1,4 @@
+        //asynchronous rendering of ReactDOM which replaces id=root elements
+        const heading = React.createElement("h2", {}, "Hello World from React!");
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(heading);
