09dc55294963d6f656c6d446213135088fc0eef1
tandamandali <tanmaykadam68@google.com>
2024-01-02 14:52:46 +0530

    correction comment

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-        //asynchronous rendering of ReactDOM which replaces id=root elements
+        // asynchronous rendering of ReactDOM which replaces id=root elements
         const heading = React.createElement("h2", {}, "Hello World from React!");
         const root = ReactDOM.createRoot(document.getElementById("root"))
         root.render(heading);
